2ca6f2da07864a8842f078ba5115f16be9fce824
2adeCO <149335018+2adeCO@users.noreply.github.com>
2025-04-22 16:31:26 +0200

    Agregar archivos de proyecto.

---
+++ Kenshi-DnD/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kenshi_DnD
+{
+    class Hero
+    {
+        //Name and title, example: "Carlos, the database scholar"
+        string name;
+        string title;
+
+        //Offensive stats
+        //Brute Force is associated with Sheks, skeletons and Hive Soldiers
+        //Skill is associated with Humans, Hive Princes and Hive Workers 
+        int bruteForce;
+        int skill;
+        //Defensive stats
+        //Health points
+        //Resistance is defense against Brute Force
+        //Perception is defense against Skill
+        int hp;
+        int resistance;
+        int perception;
+
+    }
+}
